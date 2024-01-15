cee90c8c16cec5053cb46753c90bff859859c628
Priyanka Sheoran <priynka.sheoran2004@gmail.com>
2024-01-15 11:51:01 +0530

    Initial commit

---
+++ lib/model/user_model.dart
@@ -0,0 +1,53 @@
+import 'package:user_ease/model/adderss_model.dart';
+import 'package:user_ease/model/company_model.dart';
+
+class UserEase {
+  int? id;
+  String? name;
+  String? username;
+  String? email;
+  Address? address;
+  String? phone;
+  String? website;
+  Company? company;
+
+  UserEase(
+      {this.id,
+        this.name,
+        this.username,
+        this.email,
+        this.address,
+        this.phone,
+        this.website,
+        this.company});
+
+  UserEase.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    username = json['username'];
+    email = json['email'];
+    address =
+    json['address'] != null ? new Address.fromJson(json['address']) : null;
+    phone = json['phone'];
+    website = json['website'];
+    company =
+    json['company'] != null ? new Company.fromJson(json['company']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['username'] = this.username;
+    data['email'] = this.email;
+    if (this.address != null) {
+      data['address'] = this.address!.toJson();
+    }
+    data['phone'] = this.phone;
+    data['website'] = this.website;
+    if (this.company != null) {
+      data['company'] = this.company!.toJson();
+    }
+    return data;
+  }
+}
